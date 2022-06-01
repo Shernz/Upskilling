b5647650f7721d92b991c136909bd5dcf76c4b79
Vijayalakshmy Ramakrishnan <kr.vijayalakshmy25@gmail.com>
2022-06-01 22:09:16 +0530

    Added the prompt

--- src/main/java/com/algodaily/technicalInterviewPrep/IsAnAnagram.java
+++ src/main/java/com/algodaily/technicalInterviewPrep/IsAnAnagram.java
@@ -3,6 +3,19 @@ package com.algodaily.technicalInterviewPrep;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Here's the definition of an anagram: a word, phrase, or name formed by rearranging the letters of another: such as cinema, formed from iceman.
+ *
+ * We are given two strings like "cinema" and "iceman" as inputs. Can you write a method isAnagram(str1, str2) that will return true or false depending on whether the strings are anagrams of each other?
+ *
+ * Constraints
+ * Length of both the strings <= 100000
+ * The strings will contain only alphanumeric characters (a-z , A-Z, 0-9)
+ * The strings can be empty
+ * Expected time complexity : O(nlogn)
+ * Expected space complexity : O(n)
+ */
+
 public class IsAnAnagram {
     public static boolean isAnagram(String inputA, String inputB) {
         // fill in
